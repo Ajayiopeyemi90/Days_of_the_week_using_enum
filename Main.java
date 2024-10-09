f59b8dc5a09d9144702415a353d9418eca2a10fe
Ajayi <132447599+Ajayiopeyemi90@users.noreply.github.com>
2024-10-09 16:50:16 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,51 @@
+// Creating Enum for days of the week
+enum DaysOfWeek {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
+
+// Create The Superclass
+class Week {
+    protected DaysOfWeek[] days;
+
+    public Week() {
+        days = DaysOfWeek.values();
+    }
+
+    //Create method for all days of the week
+    public void displayDays() {
+        System.out.println("Days of the Week:");
+        for (DaysOfWeek day : days) {
+            System.out.println(day);
+        }
+    }
+}
+
+// Created the Subclass
+class Weekend extends Week {
+
+    public Weekend() {
+        super(); // The supperclass constructor.
+    }
+
+    //The Method that display weekend days only
+    public void displayWeekend() {
+        System.out.println("\nWeekend Days:");
+        for (DaysOfWeek day : days) {
+            if (day == DaysOfWeek.SATURDAY || day == DaysOfWeek.SUNDAY) {
+                System.out.println(day);
+            }
+        }
+    }
+}
+
+// Main class for the model.
+public class Main {
+    public static void main(String[] args) {
+        Week week = new Week();
+        week.displayDays();
+
+        Weekend weekend = new Weekend();
+        weekend.displayWeekend();
+    }
+}
+
